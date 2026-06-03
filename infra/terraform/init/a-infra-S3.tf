df254ae1190c5f13377967e8b523d92f2d804a78
yimjongwon <awosung00@naver.com>
2026-06-03 16:27:59 +0900

    cicd_test1

---
+++ infra/terraform/init/a-infra-S3.tf
@@ -0,0 +1,41 @@
+# --------------------------------------------------
+
+provider "aws" {
+  region = "ap-northeast-2" # 서울 리전
+}
+
+# S3 버킷 이름용 랜덤 ID 생성
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+# S3 버킷 생성 - 상태 파일(.tfstate) 저장용
+resource "aws_s3_bucket" "tfstate_bucket" {
+  # 고유한 이름을 보장하기 위해 뒤에 랜덤 16진수 접미사를 붙입니다.
+  bucket        = "tfstate-bucket-${random_id.bucket_suffix.hex}"
+  force_destroy = false # 실수로 상태 파일이 담긴 버킷이 통째로 날아가는 것을 방지
+}
+
+# S3 버전 관리 활성화 (틀어지거나 유실되었을 때 과거 상태로 복구하기 위해 필수)
+resource "aws_s3_bucket_versioning" "tfstate_versioning" {
+  bucket    = aws_s3_bucket.tfstate_bucket.id
+  versioning_configuration {
+    status  = "Enabled"
+  }
+}
+
+# S3 서버 사이드 암호화 설정 (민감한 인프라 정보 암호화 보호)
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_crypto" {
+  bucket            = aws_s3_bucket.tfstate_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# S3 버킷
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.tfstate_bucket.id
+  description = "상태 파일이 저장될 S3 버킷의 이름"
+}
---
+++ infra/terraform/main.tf
@@ -0,0 +1,26 @@
+# lock 상태를 관리할 dynamodb 만들기
+terraform {
+    required_version = ">= 1.14.0" # github action 에서 에러나지 않게 일부 수정
+    required_providers {
+      aws = {
+            source  = "hashicorp/aws"
+            version = "~> 6.0" 
+      }
+    }
+    # terraform 상태 관리를 위한 remote 백엔드 설정
+    backend "s3" {
+        # 비워둔 값은 외부 파일(backend.hcl)에서 채우기
+        # bucket            = ""                                # 미리 생성한 s3 버킷의 이름
+        key                 = "lock-and-lock/terraform.tfstate" # /lock-and-lock/하위에 만들어 지도록
+        region              = "ap-northeast-2"
+        # dynamodb_table    = ""                                # 미리 준비된 dynamodb 테이블의 이름을 명시하면 lock 상태가 자동으로 관리된다.
+        encrypt             = true                              # tfstate에는 민감한 정보가 있을 수 있기 때문에 암호화
+    }
+}
+
+
+
+
+
+
+
