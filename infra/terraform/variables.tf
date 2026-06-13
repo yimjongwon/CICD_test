82c3b2f83b3b3e9a2fa3089f4a5d34eab613b925
yimjongwon <awosung00@naver.com>
2026-06-14 00:07:21 +0900

    test

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -177,18 +177,15 @@ variable "db_host_replica" {
 variable "db_user" {
   description = "DB 사용자 이름"
   type        = string
-  default     = "lb_user"
 }
 
 variable "db_password" {
   description = "DB 비밀번호"
   type        = string
   sensitive   = true
-  default     = "lb_password"
 }
 
 variable "db_name" {
   description = "DB 이름"
   type        = string
-  default     = "lb_db"
 }
